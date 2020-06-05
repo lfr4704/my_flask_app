56e0211395d846452ec7a164157b1c1ae1a8d208
Luis Robles <lfr4704@gmail.com>
2020-06-04 23:24:40 -0700

    formatting changes

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template, jsonify
 from web_app.models import db, User, Tweet, parse_records
 from web_app.services.twitter_service import api as twitter_api_client
 from web_app.services.basilica_service import connection as basilica_api_client
+
 twitter_routes = Blueprint("twitter_routes", __name__)
 @twitter_routes.route("/users/<screen_name>")
 def get_user(screen_name=None):
